efe249c05953a7b489788935a7a8a9ec3423b0af
Jing <18130288@st.hcmuaf.edu.vn>
2021-06-29 20:16:23 +0700

    header foter va ds san pham

--- ProjectBookStore/src/app/app.module.ts
+++ ProjectBookStore/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrangchuComponent } from './trangchu/trangchu.component';
+import { DanhsachspComponent } from './danhsachsp/danhsachsp.component';
+import { AppRoutingModule } from './app-routing.module'
+ import {RouterModule, Routes} from '@angular/router'
+import {TrangchuModule} from "./trangchu/trangchu.module";
+import {DanhsachspModule} from "./danhsachsp/danhsachsp.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrangchuComponent,
+    DanhsachspComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    TrangchuModule,
+    DanhsachspModule
   ],
   providers: [],
   bootstrap: [AppComponent]
