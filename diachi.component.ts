7919a1c3984056b8463af570b40b33ea5df41f78
Đức  Đào <83705299+Niss1407@users.noreply.github.com>
2021-07-04 20:05:45 +0700

    Add files via upload

---
+++ diachi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diachi',
+  templateUrl: './diachi.component.html',
+  styleUrls: ['./diachi.component.css']
+})
+export class GiohangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
